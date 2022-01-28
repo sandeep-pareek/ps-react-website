c9db634d4e194695ad1afb9329e63674bc76f20e
Sandeep Pareek <psandeep01@gmail.com>
2022-01-28 22:15:58 +0530

    Work in progress

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <br/>
+      <h3>Hi.. Welcome to <span className="c-red">pareek</span>sandeep.com!</h3>
+      <h6> Work is in progress</h6>
       <header className="App-header">
         Loading!
         <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
