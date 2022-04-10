72a4887d95705ff942135e4e92609eca1883479e
Sandeep Pareek <psandeep01@gmail.com>
2022-04-11 02:28:52 +0530

    Develop (#6)
    * Added pages for work, education, minor 1AM improvements
    
    * Added image as bg, added icons and links for github and linkedin

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import logo from '../resources/Sandeep-image.JPG';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Home() {
   return (
-    <div><br/>
-      <h3>Hi.. Welcome to <span className="c-red">pareek</span>sandeep.in!</h3>
-      <h6> Work is in progress</h6>
+    <div>
+      <Container className="home-image">
+        <br/>
+        <div id="container">
+          <div class="box" id="redbox">
+            <h1> &nbsp; Software Engineer &nbsp;</h1>
+            <h4>JAVA | SPRING | HIBERNATE | AWS | PYTHON | REACT </h4>
+          </div>
+          <div class="box" id="redbox">
+            <p class = "linkedin">&nbsp; <a href="https://www.linkedin.com/in/sandeep-pareek">LinkedIn</a></p>
+            &nbsp;<p class="github"><a href="https://github.com/sandeep-pareek">Github</a>&nbsp;</p>
+          </div>
+        </div>
+      </Container >
     </div>
   )
 }
