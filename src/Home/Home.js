9ee4978482d281daf1396d7e5ff51b0814bb1b26
Sandeep Pareek <psandeep01@gmail.com>
2022-04-23 01:21:03 +0530

    minor changes (#7)
    * Create node.js.yml (#1)
    
    * Added build status badge
    
    * Added Navbar and css
    
    * Added pages for work, education, minor 1AM improvements
    
    * Added image as bg, added icons and links for github and linkedin
    
    * SEO optimization to improve reach

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from '../resources/Sandeep-image.JPG';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
 function Home() {
   return (
